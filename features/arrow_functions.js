a4102f3380ba092037eb542ca0bf457c657e932d
smarschevski <jan.smarschevski@holsten-systems.de>
2015-09-03 22:41:46 +0200

    features "arrow_functions" and "for-of loop"

---
+++ features/arrow_functions.js
@@ -0,0 +1,84 @@
+/**
+ * Created by Jan on 26.08.2015.
+ */
+/*
+* Goal of Arrow Functions:
+* - Lexical this binding (?)
+* - shorter syntactical form for Function expressions
+*/
+
+//SYNTACTICAL DIFFERENCES -------------------------------------------------------------
+//the reduce function takes two parameters, reduce(function, 0)
+// ES 5:
+var total = values.reduce(function (a, b) {
+    return a + b;
+}, 0);
+
+// ES6
+var total = values.reduce((a, b) => a + b, 0);
+
+/* Advantages:
+ * - you can skip to write "function", "return" and braces => very practical and clean especially when you have nested function expressions *
+ * in General: () => {} vs. function () {}
+ * braces {} are used if arrow functions contain statements
+ * no braces needed if just an expression
+ * if braces => does NOT automatically return a value (only if no braces)
+ *
+  * */
+//---------------------------------------------------------------------------------------
+
+// FOR ME --------------------------------------------------------------------------------
+/* EXAMPLE FOR "THIS" (just for me)
+* var person = {
+ firstName: "Penelope",
+ lastName: "Barrymore",
+ fullName: function () {
+ console.log(this.firstName + " " + this.lastName);
+ ​// We could have also written this:​​
+ console.log(person.firstName + " " + person.lastName);
+ }
+ }
+* */
+
+// EXAMPLE FOR JS OBJECT LITERAL (comma-seperated LIST of name-value pairs)
+var Swapper = {
+    // an array literal
+    images: ["smile.gif", "grim.gif", "frown.gif", "bomb.gif"],
+    pos: { // nested object literal
+        x: 40,
+        y: 300
+    },
+    onSwap: function() { // function
+        // code here
+    }
+};
+
+ //---------------------------------------------------------------------------------------
+
+
+/*Arrow functions do not have their own "this" value
+ * The value of "this" inside an arrow function is always inherited from the enclosing scope (method)
+
+/*
+* The common "function ()" expression ALWAYS receive a this value automatically (its a property)
+* */
+
+//ES 5 name-value pair
+// you need to assign this from the addAll function to a variable self, to carry "this" into the inner method
+{
+addAll: function addAll(pieces) {
+    var self = this;
+    _.each(pieces, function (piece) {
+        self.add(piece); //can't use this, because "this" will refere to the caller of each(), which in this case is the underscore _.
+    });
+}
+}
+
+//ES 6
+// inner function is an arrow function => inherits "this" from the enclosing addAll function!
+{
+    addAll: function addAll(pieces) {
+        _.each(pieces, piece => this.add(piece));
+    }
+}
+
---
+++ features/for-of loop.js
@@ -0,0 +1,80 @@
+/**
+ * Created by Jan on 20.08.2015.
+ */
+
+/* FOR-OF LOOP  */
+
+arr = ["banana", "orange", "apple"];
+// usually: (old syntax)
+for (var i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
+//output: banana orange apple
+//-----------------------------------------------------------------------
+
+//since ES5 forEach()
+arr.forEach(function(entry){
+    console.log(entry);
+});
+//output: banana orange apple
+
+/*
+Drawbacks of forEach()
+1) you can't use break (gives you "Illegal break statement")
+e.g.  */
+for (var i = 0; i < arr.length; i++) {
+ if (i === 2) { break; }
+     console.log(arr[i]);
+ }
+//output: banana orange
+
+/*
+2) you can't return from the forEach function() (only from "function (entry)")
+e.g.
+ */
+arr = ["banana", "orange", "apple", "pie", "cheese"];
+arr.forEach(function(entry){
+    if(entry =="apple"){
+        return;
+    }
+    console.log(entry);
+
+});
+//output: banana orange pie cheese
+//-----------------------------------------------------------------------
+
+//for-in loop
+arr = ["banana", "orange", "apple"];
+arr.name = "FRUITS";
+for(var x in arr){
+    console.log(arr[x]);
+}
+
+/*
+loops over object properties (not array indices)
+    - output: banana orange apple FRUITS (arr.name is not part of the array actually, but a property)
+    - x in this case gets assigned string values "0", "1" and not 0, 1
+    - in some cases this code can loop in an arbitrary order
+ */
+
+//-----------------------------------------------------------------------
+
+//ES6 for-of loop
+for(var x of arr){
+    console.log(x);
+}
+
+/*
+- most concise, direct syntax
+- works with break, continue and return
+- loops over data (values in an array), NOT over object properties like for-in
+- works on NodeList, String, Map, Set (works with iterators)
+    -maybe examples? it starts to get very technical
+*/
+
+
+
+
+
+
+
