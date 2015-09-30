21ad03bf4a3b391733a9143b9fd97384792a6797
smarschevski <jan.smarschevski@holsten-systems.de>
2015-09-30 21:17:19 +0200

    template strings

---
+++ reveal-js/js/template-strings/template-string-2.es6.js
@@ -0,0 +1,9 @@
+var a = 3;
+var b = 5;
+
+//JS expressions in strings
+var sum = `Your total is: ${a+b}`;
+// --SNIP--
+
+export default {sum}
+
---
+++ reveal-js/js/template-strings/template-string-3.es6.js
@@ -0,0 +1,19 @@
+//multi-line strings w/o hacks
+var text = `this is
+a random
+text with whitespaces
+  over
+      multiple lines`;
+
+/*
+this is
+a random
+text with whitespaces
+  over
+      multiple lines
+ */
+
+// --SNIP--
+
+export default {}
+
